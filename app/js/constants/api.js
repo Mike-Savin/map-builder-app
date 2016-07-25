4be540e84f3e44ce0e3e07feb8469d59622773fc
Michael Savin <my.savin@yandex.com>
2016-07-25 13:06:06 +0300

    Set production hosts

--- app/js/constants/api.js
+++ app/js/constants/api.js
@@ -1,4 +1,4 @@
 var API = {
-    baseUrl: 'http://localhost:9000',
-    url: 'http://localhost:1337'
+    baseUrl: 'https://mapbuilderapp.herokuapp.com',
+    url: 'https://mapbuilderapi.herokuapp.com'
 };
